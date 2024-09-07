906b93ff216e0618f204572a8014adf8b12f083d
Krishnendu Biswas <67319318+krshnndu@users.noreply.github.com>
2024-09-07 16:41:49 +0000

    added footer

--- app.py
+++ app.py
@@ -142,5 +142,34 @@ def main():
         output_csv = test_data.to_csv(index=False)
         st.download_button(label="Download Predictions as CSV", data=output_csv, file_name='output.csv', mime='text/csv')
 
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.linkedin.com/in/krshnndu/" target="_blank">Krishnendu B</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
 if __name__ == '__main__':
     main()
